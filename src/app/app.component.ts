a2b7a128a16036cb27bbc74e9986a82e20e9e316
JaofourthsOhappy <=>
2023-10-23 01:24:40 +0700

    Binding syntax1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template:`<h1> Hello World! from inline template! </h1>
+  // <p> Hi Angular is Awesome </p>`,
+  styleUrls: ['./app.component.css'],
+  // styles:  [`h1 { color : blue;}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent {
+  
+  hotelName = 'Niku-Niku Hotel'
+}
